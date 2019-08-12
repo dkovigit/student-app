3012cba8bfa25159f3017cb691726c00106e406e
dkovigit <43017396+dkovigit@users.noreply.github.com>
2019-08-12 08:23:26 -0400

    Update SearchDTO.java

--- src/main/java/com/app/dto/SearchDTO.java
+++ src/main/java/com/app/dto/SearchDTO.java
@@ -5,9 +5,65 @@ public class SearchDTO {
   private Integer userId;
   private String searchTerm;
   private Integer requestCount;
+  private Integer start;
+  private String prefix;
+  private Integer lengthOfTerm;
+  private String country;
+  private String mode;
   
   
-  public Integer getUserId(){
+  public String getPrefix() {
+	return prefix;
+}
+
+
+public void setPrefix(String prefix) {
+	this.prefix = prefix;
+}
+
+
+public Integer getLengthOfTerm() {
+	return lengthOfTerm;
+}
+
+
+public void setLengthOfTerm(Integer lengthOfTerm) {
+	this.lengthOfTerm = lengthOfTerm;
+}
+
+
+public String getCountry() {
+	return country;
+}
+
+
+public void setCountry(String country) {
+	this.country = country;
+}
+
+
+public String getMode() {
+	return mode;
+}
+
+
+public void setMode(String mode) {
+	this.mode = mode;
+}
+
+  
+  
+  public Integer getStart() {
+	return start;
+}
+
+
+public void setStart(Integer start) {
+	this.start = start;
+}
+
+
+public Integer getUserId(){
 	     return this.userId;
 	  }
 	  
