d5e92890b67a6f92b9440a067ca8411701c55b22
dkovigit <43017396+dkovigit@users.noreply.github.com>
2019-08-25 15:16:29 -0400

    Update SearchService.java

--- src/main/java/com/app/service/SearchService.java
+++ src/main/java/com/app/service/SearchService.java
@@ -6,15 +6,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.app.dto.SearchDTO;
+import com.app.dto.SearchResponseDTO;
 import com.app.exception.CustomException;
 import com.app.model.Search;
 import com.app.repository.SearchRepository;
+import com.app.service.SolrSearchService;
 
 @Service
 public class SearchService {
 
 	 @Autowired
 	  private SearchRepository searchRepository;
+	 
+	 @Autowired
+	 SolrSearchService solrSearchService;
 
 	  public void saveSearch(SearchDTO searchDto) {
 	    try {
@@ -27,6 +32,17 @@ public class SearchService {
 	    }
 	  }
 	  
+	  
+	  public SearchResponseDTO performSearch(SearchDTO searchDto) {
+		    SearchResponseDTO searchResponseDto;
+		  try {
+			  searchResponseDto = solrSearchService.runSearch(searchDto); 
+			  saveSearch(searchDto);
+			  return searchResponseDto;
+		     } catch (RuntimeException e) {
+		      throw new CustomException("Search Request Failed", HttpStatus.UNPROCESSABLE_ENTITY);
+		    }
+		  }
 	  
 
 	 
